45f670132d0367001986d8bc11cbca75692c0443
Bryan <35320836+potentpwnables@users.noreply.github.com>
2023-03-04 12:05:54 -0500

    Fixes the snort3_libdaq_latest_url
    The URL for the `libdaq` tar.gz file has a "v" before the version number, which the `$snort3_libdaq_version_string` variable doesn't contain. This PR adds the v into the URL so that `libdaq` can be successfully installed.

--- Ubuntu/AVATAR/autosnort3-Ubuntu.sh
+++ Ubuntu/AVATAR/autosnort3-Ubuntu.sh
@@ -207,7 +207,7 @@ snort3_latest_url="https://github.com/snort3/snort3/archive/refs/tags/$snort3_ve
 snort3_libdaq_tarball=`grep libdaq- downloads | cut -d">" -f2 | cut -d "<" -f1`
 snort3_libdaq_version_string=`echo $snort3_libdaq_tarball | cut -d"-" -f2`
 snort3_libdaq_dirstring=`echo $snort3_libdaq_tarball | sed 's/.tar.gz//' | sed 's/v//'`
-snort3_libdaq_latest_url="https://github.com/snort3/libdaq/archive/refs/tags/$snort3_libdaq_version_string"
+snort3_libdaq_latest_url="https://github.com/snort3/libdaq/archive/refs/tags/v$snort3_libdaq_version_string"
 
 snort3_extras_tarball=`grep snort3_extra downloads | cut -d">" -f2 | cut -d "<" -f1`
 snort3_extras_version_string=`echo $snort3_extras_tarball | cut -d"-" -f2`
